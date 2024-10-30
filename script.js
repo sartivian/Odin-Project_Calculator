3cd8b12e3012b2f219c2a8127c92e2fd81b99c53
Sartivian <fsartivian@gmail.com>
2024-10-30 17:15:33 +0700

    Create the basics

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstNumber = 0
+let operator = 0
+let secondNumber = 0
+
+
+// add
+function add(firstNumber,secondNumber){
+    return firstNumber + secondNumber
+}
+// subtract
+function substract(firstNumber,secondNumber){
+    return firstNumber - secondNumber
+}
+// multiply
+function multiply(firstNumber,secondNumber){
+    return firstNumber * secondNumber
+}
+// divide
+function divide(firstNumber,secondNumber){
+    return firstNumber / secondNumber
+}
+
+//operate function
+function operate(){
+    
+}
